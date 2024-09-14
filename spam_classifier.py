a32c62afaaead861b7031b76f0c8b03f65350469
Zaeem Hassan <zaeemhassan2004@gmail.com>
2024-09-15 00:44:40 +0500

    initial commit

---
+++ spam_classifier.py
@@ -0,0 +1,44 @@
+import streamlit as st 
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
+ps = PorterStemmer()
+def transform(text):
+     text =  text.lower()
+     text =  nltk.word_tokenize(text)
+     y = []
+     for i in text:
+          if i.isalnum():
+              y.append(i)
+     text =  y[:]
+     y.clear()
+     for i in text :
+          if i not in stopwords.words('english') and i not in  string.punctuation:
+             y.append(i)
+     text = y[:]
+     y.clear()
+     for  i in text:
+        y.append(ps.stem(i)) 
+     return  " ".join(y) 
+     #return text
+tf = pickle.load(open("vectorizer.pkl",'rb'))
+model =pickle.load(open("model.pkl","rb"))
+st.title("Email Spam Classifier")
+input_sms=st.text_area("Enter the Message")
+if st.button('Predict'):
+    #Preprocess
+    transform_text = transform(input_sms)
+
+    #Vectorize 
+
+    vector_input = tf.transform([transform_text])
+    # Predict 
+    result = model.predict(vector_input)[0]
+    if result ==1:
+        st.header("Spam")
+    else :
+        st.header("Not Spam")
